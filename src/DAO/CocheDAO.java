f6604f8d29b2bfbcd05c6fb2a989d68a8c60bb93
xikoto <josemibeni@gmail.com>
2015-12-13 01:01:58 +0100

    Solucionados los problemas con listarCochesPorSucursal

--- src/DAO/CocheDAO.java
+++ src/DAO/CocheDAO.java
@@ -8,37 +8,37 @@ import DAO.dto.CocheDTO;
 import UTIL.ConnectionManager;
 import UTIL.DAOExcepcion;
 
-public class CocheDAO extends UtilDAO implements ICocheDAO{
-	protected ConnectionManager connManager;
-	
-	public CocheDAO() throws DAOExcepcion {
-		super();
-		try {
-			connManager = new ConnectionManager("alquilervehiculosBD");
-		} catch (ClassNotFoundException e) {
-			throw new DAOExcepcion(e);
-		}
-	}
+public class CocheDAO implements ICocheDAO{
+    protected ConnectionManager connManager;
+    
+    public CocheDAO() throws DAOExcepcion {
+        super();
+        try {
+            connManager = new ConnectionManager("alquilervehiculosBD");
+        } catch (ClassNotFoundException e) {
+            throw new DAOExcepcion(e);
+        }
+    }
 
-	@Override
-	public ArrayList<CocheDTO> obtenerCochesSucursal(int idSucrursal) throws DAOExcepcion{
-		try{
-			connManager.connect();
-			ResultSet rs=connManager.queryDB("select * from COCHE where sucursal="+idSucrursal);
-			connManager.close();
-			String matricula, categoria;
-			ArrayList<CocheDTO> listac=new ArrayList<CocheDTO>();
-			while(rs.next()){
-				matricula = limpiarString(rs.getString("matricula"));
-				categoria = limpiarString(rs.getString("nombre"));
-				listac.add(new CocheDTO(matricula, 
-										rs.getDouble("kmsActuales"), 
-										rs.getInt("sucursal"), 
-										categoria));
-			}	
-			return listac;
-		}
-		catch (SQLException e){	throw new DAOExcepcion(e);}	
-	}
+    @Override
+    public ArrayList<CocheDTO> obtenerCochesSucursal(int idSucrursal) throws DAOExcepcion{
+        try{
+            connManager.connect();
+            ResultSet rs=connManager.queryDB("select * from COCHE where sucursal="+idSucrursal);
+            connManager.close();
+            ArrayList<CocheDTO> listac=new ArrayList<CocheDTO>();
+            while(rs.next()){
+                listac.add(new CocheDTO(rs.getString("matricula"), 
+                                        rs.getDouble("kmsActuales"), 
+                                        rs.getInt("sucursal"), 
+                                        rs.getString("nombre")));
+            }
+            
+            if(listac.isEmpty())throw new DAOExcepcion("No existen registros de coches para la sucursal: "+ idSucrursal);
+            return listac;
+        }
+        catch (SQLException e){    throw new DAOExcepcion(e);}    
+    }
 
 }
+
